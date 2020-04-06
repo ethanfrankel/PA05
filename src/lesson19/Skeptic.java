88b38315f071f1edaf731a83304ebf8b887e79b4
joseph-moniz <58448341+joseph-moniz@users.noreply.github.com>
2020-04-05 20:14:21 -0400

    First step of Part I (Joseph)

---
+++ src/lesson19/Skeptic.java
@@ -0,0 +1,14 @@
+package lesson19;
+
+public class Skeptic extends Person {
+
+	public Skeptic() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void tryToMove() {
+		tryToMoveRandomly();
+	}
+
+}
