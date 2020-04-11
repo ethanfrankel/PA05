d7be76e2ffe36b22bcefa7fdbb4960700ca9c7a7
dantegoss <55816621+dantegoss@users.noreply.github.com>
2020-04-11 09:42:40 -0400

    Update AnalyzeSimulation.java

--- src/lesson19/AnalyzeSimulation.java
+++ src/lesson19/AnalyzeSimulation.java
@@ -2,19 +2,26 @@ package lesson19;
 public class AnalyzeSimulation{
     //runs will be the number of times it loops (one hundred). 
     public int runs=0;
+    //public int increment=1; 
     //Assume a tick is one day
     public int totalnumOfDays=100000; 
     public int totalnumOfInfected=0; 
     public int totalpeakInfectedLevel=0;
     
     public AnalyzeSimulation(int runs){
-        runs=this.runs;
+        this.runs = runs;
+        /*this.runs = 0;
+        *
+        * while (this.runs != runs) {
+        *   this.runs+=increment;
+        *   }
+        */
     }
     public void setinfected(int infected){
-        totalnumOfInfected=this.totalnumOfInfected+infected;
+        this.totalnumOfInfected=this.totalnumOfInfected+infected;
     }
     public void setdays(int days){
-        totalnumOfDays=this.totalnumOfDays+days;
+        tthis.totalnumOfDays=this.totalnumOfDays+days;
     }
     public void getaveragestats(){
         double avginfected=this.totalnumOfInfected/this.totalnumOfDays;
